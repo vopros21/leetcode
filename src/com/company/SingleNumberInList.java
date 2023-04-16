9f09b8fadb5face965af078415ebf7a2c39f859d
Mike Kostenko <vopros.m@gmail.com>
2023-04-16 20:52:52 +0100

    SingleNumberInList solution for leetcode

--- src/com/company/SingleNumberInList.java
+++ src/com/company/SingleNumberInList.java
@@ -2,15 +2,20 @@ package com.company;
 
 /**
  * <a href="https://leetcode.com/problems/single-number/">Single number</a>
- *  * Runtime: 0ms, beats: 100%; Memory: 43.6MB, beats: 21.6%
+ *  * Runtime: 1ms, beats: 100%; Memory: 42.2MB, beats: 90.85%
  * @author Mike Kostenko on 16/04/2023
  */
 public class SingleNumberInList {
     public int singleNumber(int[] nums) {
-        return 0;
+        int res = 0;
+        for (int i : nums){
+            res ^= i;
+        }
+        return res;
     }
 
     public static void main(String[] args) {
         int[] numbers = {2, 2, 1, 3, 1, 4, 4};
+        System.out.println(new SingleNumberInList().singleNumber(numbers));
     }
 }
