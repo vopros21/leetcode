bdc6301c5fc0a46437f6084febec7a17fcdef638
Mike Kostenko <vopros.m@gmail.com>
2023-04-16 20:43:18 +0100

    SingleNumberInList template for leetcode

---
+++ src/com/company/SingleNumberInList.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * <a href="https://leetcode.com/problems/single-number/">Single number</a>
+ *  * Runtime: 0ms, beats: 100%; Memory: 43.6MB, beats: 21.6%
+ * @author Mike Kostenko on 16/04/2023
+ */
+public class SingleNumberInList {
+    public int singleNumber(int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 2, 1, 3, 1, 4, 4};
+    }
+}
