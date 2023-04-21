d48b62ad08e12df5459bdd8350cd4185854a15d4
Mike Kostenko <vopros.m@gmail.com>
2023-04-21 19:12:11 +0100

    Majority element for leetcode

---
+++ src/com/company/MajorityElement.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.HashMap;
+
+/**
+ * <a href="https://leetcode.com/problems/majority-element/">Majority Element</a>
+ *  * Runtime: ms, beats: %; Memory: MB, beats: %
+ * @author Mike Kostenko on 21/04/2023
+ */
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i: nums) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        return map.entrySet().stream().max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getKey();
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2,2,1,1,1,2,2};
+        System.out.println(new MajorityElement().majorityElement(numbers));
+    }
+}
